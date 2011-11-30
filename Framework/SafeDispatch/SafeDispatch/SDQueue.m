6c040ca604577b6eeb10045dcd0a2a353429257a
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-29 21:39:14 -0800

    Quickly hacking an implementation for determining whether a queue is currently running (before I forget it)

--- Framework/SafeDispatch/SafeDispatch/SDQueue.m
+++ Framework/SafeDispatch/SafeDispatch/SDQueue.m
@@ -8,6 +8,35 @@
 
 #import <SafeDispatch/SDQueue.h>
 
+typedef struct sd_dispatch_queue_stack {
+    dispatch_queue_t queue;
+    struct sd_dispatch_queue_stack *next;
+} sd_dispatch_queue_stack;
+
+// used with dispatch_set_queue_specific()
+static const void * const SDDispatchQueueStackKey = "SDDispatchQueueStack";
+
+@interface SDQueue () {
+    dispatch_queue_t m_dispatchQueue;
+}
+
+@property (readonly, getter = isCurrentQueue) BOOL currentQueue;
+@end
+
 @implementation SDQueue
+
+#pragma mark Properties
+
+- (BOOL)isCurrentQueue {
+    sd_dispatch_queue_stack *stack = dispatch_get_specific(SDDispatchQueueStackKey);
+    while (stack) {
+        if (stack->queue == m_dispatchQueue)
+            return YES;
+
+        stack = stack->next;
+    }
+
+    return NO;
+}
 
 @end
