2da330c19d030f9d91c9383cb4d0663d0f772248
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-29 20:47:09 -0800

    Changed copyrights to public domain

--- Framework/SafeDispatch/SafeDispatch/SafeDispatch.m
+++ Framework/SafeDispatch/SafeDispatch/SafeDispatch.m
@@ -3,7 +3,7 @@
 //  SafeDispatch
 //
 //  Created by Justin Spahr-Summers on 29.11.11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Released into the public domain.
 //
 
 #import "SafeDispatch.h"
