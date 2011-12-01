371669a7db67268df9e8a2671a9660718d272260
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-30 19:50:40 -0800

    Added some simple unit tests for SDGroup

---
+++ Framework/SafeDispatch/SafeDispatchTests/SDGroupTests.h
@@ -0,0 +1,13 @@
+//
+//  SDGroupTests.h
+//  SafeDispatch
+//
+//  Created by Justin Spahr-Summers on 30.11.11.
+//  Released into the public domain.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SDGroupTests : SenTestCase
+
+@end
---
+++ Framework/SafeDispatch/SafeDispatchTests/SDGroupTests.m
@@ -0,0 +1,65 @@
+//
+//  SDGroupTests.m
+//  SafeDispatch
+//
+//  Created by Justin Spahr-Summers on 30.11.11.
+//  Released into the public domain.
+//
+
+#import "SDGroupTests.h"
+#import <SafeDispatch/SafeDispatch.h>
+#import <libkern/OSAtomic.h>
+
+@implementation SDGroupTests
+
+- (void)testSimpleGroup {
+    __block BOOL doneA = NO;
+    __block BOOL doneB = NO;
+
+    SDQueue *queue = [SDQueue concurrentGlobalQueue];
+    SDGroup *group = [[SDGroup alloc] initWithDestinationQueue:queue];
+
+    [group runAsynchronously:^{
+        [NSThread sleepForTimeInterval:0.01];
+        doneA = YES;
+    }];
+
+    [group runAsynchronously:^{
+        doneB = YES;
+        [NSThread sleepForTimeInterval:0.01];
+    }];
+
+    [group wait];
+
+    OSMemoryBarrier();
+    STAssertTrue(doneA, @"");
+    STAssertTrue(doneB, @"");
+}
+
+- (void)testNotification {
+    __block BOOL done = NO;
+    __block BOOL notified = NO;
+
+    SDQueue *queue = [[SDQueue alloc] init];
+    SDGroup *group = [[SDGroup alloc] initWithDestinationQueue:queue];
+
+    [group runAsynchronously:^{
+        [NSThread sleepForTimeInterval:0.01];
+        done = YES;
+    }];
+
+    [group runWhenCompleted:^{
+        STAssertTrue(done, @"");
+        notified = YES;
+    }];
+
+    [group wait];
+    [NSThread sleepForTimeInterval:0.05];
+
+    [queue runBarrierSynchronously:^{
+        STAssertTrue(done, @"");
+        STAssertTrue(notified, @"");
+    }];
+}
+
+@end
